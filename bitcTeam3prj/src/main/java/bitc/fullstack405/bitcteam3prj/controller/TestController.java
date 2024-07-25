7ecb763924368bf9bda011aaf875390707d12540
bycheon96 <byeongchan0805@daum.net>
2024-07-25 14:29:51 +0900

    사용가능 HTML 리스트 header,footer boardDetail List mainHome movieinfo login sign in

--- bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/controller/TestController.java
+++ bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/controller/TestController.java
@@ -3,15 +3,12 @@ package bitc.fullstack405.bitcteam3prj.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
 
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
     public String test(){
-        return "/board/login_info.html";
+        return "board/boardDetail";
     }
-
-
 }
