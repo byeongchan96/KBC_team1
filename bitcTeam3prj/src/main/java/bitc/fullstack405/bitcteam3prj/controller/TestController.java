2a7dd39ec758c84a74e8275d64a274b581cc9417
bycheon96 <byeongchan0805@daum.net>
2024-07-25 18:32:19 +0900

    main view 수정 후 사용해야됨

--- bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/controller/TestController.java
+++ bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
     public String test(){
-        return "board/boardDetail";
+        return "login/Login.html";
     }
 }
