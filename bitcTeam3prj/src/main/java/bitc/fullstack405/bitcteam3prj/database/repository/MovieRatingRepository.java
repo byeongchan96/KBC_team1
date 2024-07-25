6843a290fce8c5177d34131a441fb47196df7f99
moranengyo <moranengyo@gmail.com>
2024-07-25 17:29:40 +0900

    Revert "movieRatingService 생성 및 구현(Impl)"
    This reverts commit 7bea857121c20722b00060921cdb56afa10fb6ef.

--- bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/database/repository/MovieRatingRepository.java
+++ bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/database/repository/MovieRatingRepository.java
@@ -4,8 +4,6 @@ import bitc.fullstack405.bitcteam3prj.database.entity.MovieBoardEntity;
 import bitc.fullstack405.bitcteam3prj.database.entity.MovieBoardRatingEntity;
 import bitc.fullstack405.bitcteam3prj.database.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,7 +13,5 @@ public interface MovieRatingRepository extends JpaRepository<MovieBoardRatingEnt
 
   void deleteByUser(UserEntity user);
 
-  @Query("select round(avg(mbr.movieRating), 0) as avgRating from MovieBoardRatingEntity as mbr where mbr.movieBoard = :movieBoard")
-  MovieBoardRatingEntity querySelectAvgRating(@Param("movieBoard") MovieBoardEntity id);
-
+  
 }
--- bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/service/RatingService.java
+++
@@ -1,7 +0,0 @@
-package bitc.fullstack405.bitcteam3prj.service;
-
-public interface RatingService {
-  void findAllByMovieBoard() throws Exception;
-  void deleteByUser() throws Exception;
-  void querySelectAvgRating() throws Exception;
-}
