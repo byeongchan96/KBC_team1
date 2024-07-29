959b0df81693386c2e105685a491a17d04714b62
moranengyo <moranengyo@gmail.com>
2024-07-29 19:56:54 +0900

    MovieRating > 평점 등록 / 수정 / 삭제 기능 추가

--- bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/controller/MovieBoardController.java
+++ bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/controller/MovieBoardController.java
@@ -3,9 +3,8 @@ package bitc.fullstack405.bitcteam3prj.controller;
 
 import bitc.fullstack405.bitcteam3prj.database.entity.*;
 import bitc.fullstack405.bitcteam3prj.database.repository.MovieRatingRepository;
-import bitc.fullstack405.bitcteam3prj.service.MovieBoardService;
-import bitc.fullstack405.bitcteam3prj.service.MovieService;
-import bitc.fullstack405.bitcteam3prj.service.RatingService;
+import bitc.fullstack405.bitcteam3prj.service.*;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -26,8 +25,10 @@ public class MovieBoardController {
 
     @Autowired
     private RatingService ratingService;
+    @Autowired
+    private MovieRatingRepository movieRatingRepository;
   @Autowired
-  private MovieRatingRepository movieRatingRepository;
+  private UserService userService;
 
 
     @GetMapping({"/", ""})
@@ -70,40 +71,54 @@ public class MovieBoardController {
         return mv;
     }
 
+    @GetMapping("/{movieBoardId}/rating")
+    public String writeMovieRating() throws Exception {
+        return "/movie/movieinfo/{movieBoardId}";
+    }
 
-    @PostMapping("/{movieBoardId}/rating")
-    public String writeMovieRating(@PathVariable("movieBoardId") long movieBoardId, MovieBoardRatingEntity ratingEntity) throws Exception {
+    @PostMapping("/{movieBoardId}/rating/{userId}")
+    public String writeMovieRating(
+            @PathVariable("userId") String userId,
+            @PathVariable("movieBoardId") long movieBoardId,
+            MovieBoardRatingEntity ratingEntity) throws Exception {
 
+        var user = userService.findByUserId(userId);
         ratingEntity.setMovieBoard(movieBoardService.selectMovieBoardDetail(movieBoardId));
-//        ratingEntity.setMovieRating(movieRating);
-//        ratingEntity.setContent(content);
+        ratingEntity.setUser(user);
 
         ratingService.insertRating(ratingEntity);
-        ;
 
-        return "redirect:/movie/movieinfo/{movieBoardId}";
+        return "redirect:/movie/movieinfo/" + movieBoardId;
     }
 
-//    @PostMapping("/{movieBoardId}/rating")
-//    public String writeRating(@PathVariable("movieBoardId") long movieBoardId, @RequestParam("movieRating") int movieRating, @RequestParam("content") String content) throws Exception {
-//
-//        MovieBoardRatingEntity entity = new MovieBoardRatingEntity();
-//
-//        entity.setMovieBoard((MovieBoardEntity)movieBoardId);
-//
-//        ratingService.insert();
-//
-//    }
 
+  @PostMapping("/movieinfo/update/{movieBoardRatingId}")
+  public String updateMovieRating(
+          @PathVariable("movieBoardRatingId") long movieBoardRatingId,
+                        MovieBoardRatingEntity ratingEntity,
+                        long movieBoardId, String userId) throws Exception {
 
-    @ResponseBody
-    @DeleteMapping("/{movieBoardId}/rating/{ratingId}")
-    public Object deleteMovieRating(
-            @PathVariable("movieBoardId") long movieBoardId,
-            @PathVariable("ratingId") long ratingId) throws Exception {
+    ratingEntity.setId(movieBoardRatingId);
 
+    MovieBoardEntity movieBoardEntity = movieBoardService.selectMovieBoardDetail(movieBoardId);
+    UserEntity userEntity = userService.findByUserId(userId);
+    ratingEntity.setMovieBoard(movieBoardEntity);
+    ratingEntity.setUser(userEntity);
 
-        return null;
+    ratingService.updateRating(ratingEntity);
+
+    return "redirect:/movie/movieinfo/" + movieBoardId;
+  }
+
+    @PostMapping("/movieinfo/delete/{movieBoardRatingId}")
+    public String deleteMovieRating(
+            @PathVariable("movieBoardRatingId") long movieBoardRatingId,
+            long movieBoardId) throws Exception {
+
+            ratingService.deleteById(movieBoardRatingId);
+
+        return "redirect:/movie/movieinfo/" + movieBoardId;
     }
+
 
 }
