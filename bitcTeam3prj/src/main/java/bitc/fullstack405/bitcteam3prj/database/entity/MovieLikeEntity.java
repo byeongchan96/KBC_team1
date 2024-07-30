8cf0b8e7de36f386ab245d5e8e53dcc86e1afbb4
moranengyo <moranengyo@gmail.com>
2024-07-30 20:23:29 +0900

    <MovieLikeEntity> 이전 내용과 같음

--- bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/database/entity/MovieLikeEntity.java
+++ bitcTeam3prj/src/main/java/bitc/fullstack405/bitcteam3prj/database/entity/MovieLikeEntity.java
@@ -9,11 +9,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Data
-@Table(name = "board_like")
+@Table(name = "movie_like")
 @NoArgsConstructor
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class BoardLikeEntity {
+public class MovieLikeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id; // 게시물 추천/비추천 id, pk, 사용자,게시글 fk
@@ -24,9 +24,7 @@ public class BoardLikeEntity {
     private UserEntity user;
 
     @ManyToOne
-    @JoinColumn(name="board_id")
-    private BoardEntity board;
+    @JoinColumn(name="movie_id")
+    private MovieEntity movie;
 
-    @Column
-    private String likeYn;
 }
